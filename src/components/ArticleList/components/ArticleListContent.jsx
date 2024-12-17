d0fdce1bd21c71f88f7b0805493fe91c46c2297f
lisicheng <noteperson@gmail.com>
2024-12-17 17:46:34 +0800

    文章列表动画

--- src/components/ArticleList/components/ArticleListContent.jsx
+++ src/components/ArticleList/components/ArticleListContent.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { filter } from "@/stores/articlesStore.js";
 import { useStore } from "@nanostores/react";
 import { Virtuoso } from "react-virtuoso";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ArticleItem = memo(({ article, isLast }) => (
   <div className="mx-2">
@@ -24,21 +25,38 @@ export default function ArticleListContent({ articles }) {
   let info = [feedId, categoryId, $filter].filter(Boolean).join("-");
 
   return (
-    <Virtuoso
-      className="article-list-content flex-1 h-full"
-      data={articles}
-      components={{
-        Header: () => <div className="vlist-header h-16"></div>,
-        Footer: () => <div className="vlist-footer h-16"></div>,
-      }}
-      itemContent={(index, article) => (
-        <ArticleItem
-          key={article.id}
-          article={article}
-          isLast={index === articles.length - 1}
-          info={info}
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={info}
+        initial={{ y: 50, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            type: "spring",
+            bounce: 0.3,
+            opacity: { delay: 0.05 }
+          }
+        }}
+        exit={{ y: -50, opacity: 0 }}
+        className="article-list-content flex-1 h-full"
+      >
+        <Virtuoso
+          data={articles}
+          components={{
+            Header: () => <div className="vlist-header h-16"></div>,
+            Footer: () => <div className="vlist-footer h-16"></div>,
+          }}
+          itemContent={(index, article) => (
+            <ArticleItem
+              key={article.id}
+              article={article}
+              isLast={index === articles.length - 1}
+              info={info}
+            />
+          )}
         />
-      )}
-    />
+      </motion.div>
+    </AnimatePresence>
   );
 }
