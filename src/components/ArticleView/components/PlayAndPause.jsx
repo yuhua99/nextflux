9d42ad6bd6b766dcb7c1d3105ced6fad7f6d2271
lisicheng <noteperson@gmail.com>
2024-12-20 11:13:39 +0800

    全局音乐状态（WIP）

--- src/components/ArticleView/components/PlayAndPause.jsx
+++ src/components/ArticleView/components/PlayAndPause.jsx
@@ -9,13 +9,18 @@ import {
 import { activeArticle } from "@/stores/articlesStore.js";
 import { useStore } from "@nanostores/react";
 import { Pause, Play } from "lucide-react";
-import { extractFirstImage } from "@/lib/utils.js";
+import { cn, extractFirstImage } from "@/lib/utils.js";
 
 export default function PlayAndPause({ source }) {
   const $paused = useStore(paused);
   const $activeArticle = useStore(activeArticle);
   return (
-    <Card className="playAndPause mx-auto w-60 aspect-square max-w-full bg-content2">
+    <Card
+      className={cn(
+        "playAndPause mx-auto w-60 aspect-square max-w-full bg-content2",
+        !$paused && "scale-105",
+      )}
+    >
       <CardHeader className="absolute z-10 top-1 w-full h-full flex-col items-center justify-center">
         <Button
           className="bg-white text-default-500 shadow-custom"
@@ -40,10 +45,7 @@ export default function PlayAndPause({ source }) {
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src={
-          extractFirstImage($activeArticle) ||
-          ""
-        }
+        src={extractFirstImage($activeArticle) || ""}
       />
     </Card>
   );
