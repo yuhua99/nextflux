5559d2683b002e6eeab69ef003d4c7e3a3f5e51b
lisicheng <noteperson@gmail.com>
2025-01-13 17:35:20 +0800

    常规调整

--- src/components/ArticleView/ArticleView.jsx
+++ src/components/ArticleView/ArticleView.jsx
@@ -189,7 +189,7 @@ const ArticleView = () => {
               {audioEnclosure && <PlayAndPause source={audioEnclosure} />}
               <PhotoProvider
                 bannerVisible={false}
-                maskOpacity={0.5}
+                maskOpacity={0.8}
                 loop={false}
                 speed={() => 300}
                 easing={(type) =>
